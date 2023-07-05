dbe3e608e5942202d007c946dd39b38553b7a3fd
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-06 00:34:39 +0400

    task 6_1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,24 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+
+        String furValue = hasFur ? "a" : "no";
+        String pawNumber = numberOfPaws > 1 ? "paws" : "paw";
+
+        return String.format("This animal is mostly %s. " +
+                "It has %d %s and %s fur.", color, numberOfPaws, pawNumber, furValue);
+    }
+
+
 }
